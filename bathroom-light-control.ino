6f20f34d81dca001cc3d84c70a81f58174516a04
JYewman <joshua.yewman@outlook.com>
2022-10-09 20:55:45 +0100

    Changes to be committed: 	modified:   bathroom-light-control.ino

--- bathroom-light-control.ino
+++ bathroom-light-control.ino
@@ -76,15 +76,10 @@ void loop()
                     client.println("HTTP/1.1 200 OK");
                     client.println("Content-Type: text/html");
                     client.println("Connection: close"); // the connection will be closed after completion of the response
-                    client.println("Refresh: 5");        // refresh the page automatically every 5 sec
                     client.println();
                     client.println("<!DOCTYPE HTML>");
                     client.println("<html>");
-                    // output the value of each analog input pin
-                    for (int analogChannel = 0; analogChannel < 6; analogChannel++)
-                    {
-                        client.print("<h1>Hello world!</h1>");
-                    }
+                    client.print("<h1>Hello world!</h1>");
                     client.println("</html>");
                     break;
                 }
